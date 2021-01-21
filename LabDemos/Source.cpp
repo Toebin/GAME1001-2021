cc76fe150eeb6e394b3461ddd92da7a60cb9c661
Mitchell <mitchell.tobin@georgebrown.ca>
2021-01-20 21:56:39 -0500

    Completed Week 2 Demo

--- LabDemos/Source.cpp
+++ LabDemos/Source.cpp
@@ -1,13 +1,21 @@
+// A simple C++ program
 #include <iostream>
-using namespace std;
+#include<string>
+using namespace std;  // Use the standard namespace
 
-
-//this is a comment
+//Main entry point to the application
 int main()
 {
-	int x = 5;
+	double first = 5.25,
+		second = 51.7,
+		third = 20.125,
+		fourth = -10.4;
 
-	cout << x;
+	cout << "Addition: " << first + second << endl;
+	cout << "Subtraction: " << second - third << endl;
+	cout << "Multiplication: " << third * fourth << endl;
+	cout << "Division: " << fourth / first << endl;
+	cout << "Modulus: " << (int)first % (int)second << endl;
 
 	return 0;
 }
